0ce5c99d77796d19bafc1e0605d964f178001a62
malikmasis <malik.masis@gmail.com>
2018-02-09 23:21:13 +0300

    Add project files.

---
+++ Counter/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Counter.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult Register()
+        {
+            ViewBag.Message = "Your Register page.";
+
+            return View();
+        }
+
+        public ActionResult AboutA()
+        {
+            ViewBag.Message = "Your application description page.A";
+
+            return View();
+        }
+    }
+}
